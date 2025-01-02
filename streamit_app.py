7b321f8de87528662e43aa20c254b237fd583379
LapisSapphire <ergosophie@protonmail.com>
2025-01-02 16:18:22 -0600

    Update streamit_app.py

--- streamit_app.py
+++ streamit_app.py
@@ -33,7 +33,6 @@ ingredients_list = st.multiselect (
     'Choose up to 5 ingredients:', my_dataframe 
     , max_selections = 5
     ) 
-
 if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
